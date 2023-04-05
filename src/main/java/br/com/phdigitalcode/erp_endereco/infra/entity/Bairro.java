4dc6257103cebfd856ac21bdd8b6770f841cc636
phelippnd-programador <phelippnd.programador@gmail.com>
2023-04-05 01:21:13 -0300

    Ajustado entidade

--- src/main/java/br/com/phdigitalcode/erp_endereco/infra/entity/Bairro.java
+++ src/main/java/br/com/phdigitalcode/erp_endereco/infra/entity/Bairro.java
@@ -21,7 +21,7 @@ public class Bairro implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	@ManyToOne
 	private Municipio municipio;
